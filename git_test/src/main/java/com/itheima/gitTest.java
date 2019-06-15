f727f0076c7cc14d15b5facdcc04f4937443c919
Dade <1097287498@qq.com>
2019-06-15 17:28:55 +0800

    第三次提交

--- git_test/src/main/java/com/itheima/gitTest.java
+++ git_test/src/main/java/com/itheima/gitTest.java
@@ -1,5 +1,7 @@
 package com.itheima;
 
+import org.junit.Test;
+
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
@@ -9,7 +11,12 @@ public class gitTest {
         System.out.println("hello,git");
         System.out.println("hello,git");
     }
-
-
+    @Test
+    public void test(){
+        System.out.println("hhh");
+        System.out.println("hhh");
+        System.out.println("hhh");
+        System.out.println("hhh");
+    }
 
 }
